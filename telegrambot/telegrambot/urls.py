47c540474fca2653202efc5559a5bc93948edf84
Crack49 <emiliovirtual@hotmail.com>
2016-03-15 20:52:53 +0100

    Hardcore structure change
    Not to do it again ;:D

--- telegrambot/telegrambot/urls.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from django.conf.urls import include, url
-
-urlpatterns = [
-    url(r'^admin/', admin.site.urls),
-    url(r'^diferentes/', include('diferentes.urls')),
-]
